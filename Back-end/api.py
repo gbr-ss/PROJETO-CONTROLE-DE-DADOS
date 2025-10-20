18a49919ef2ae31e60cf88e98f515a014659c21e
gabriel santos silva <gabriel.s.silva95@aluno.senai.br>
2025-10-20 10:31:55 -0300

    add api post criar_produto

--- Back-end/api.py
+++ Back-end/api.py
@@ -1,7 +1,12 @@
 from fastapi import FastAPI
 import funcao
-app = FastAPI(title="Gerenciador de Filmes") 
+app = FastAPI(title="Gerenciador de Produtos") 
 
 @app.get("/")
 def home():
     return {"mensagem": "Bem-Vindo ao Gerenciador de Produtos e Estoque"}
+
+@app.post("/produtos")
+def criar_produto(nome: str, categoria: str, preco: int, quantidade: float):
+    funcao.inserir_produtos(nome, categoria, preco ,quantidade)
+    return{"mensagem": "Produto adicionado com sucesso"}
