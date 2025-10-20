c3073b72c4dc86b77b161265be260fb4475e8df8
gabriel santos silva <gabriel.s.silva95@aluno.senai.br>
2025-10-20 10:14:21 -0300

    add função atualizar

--- Back-end/funcao.py
+++ Back-end/funcao.py
@@ -9,7 +9,7 @@ def criar_tabela():
             id SERIAL PRIMARY KEY,
             nome VARCHAR(100) NOT NULL,
             categoria VARCHAR(50),
-            preco DECIMAL(10,2),
+            preco NUMERIC(10,2),
             quantidade INT
             );
             """)
@@ -46,6 +46,21 @@ def exibir_produtos():
             return cursor.fetchall()
         except Exception as erro:
             print(f"Erro ao mostrar os produtos")
+        finally:
+            cursor.close()
+            conexao.close()
+
+def atualizar_produtos(id_produtos, nova_quantidade):
+    conexao, cursor = conectar()
+    if conexao:
+        try:
+            cursor.execute(
+                "UPDATE produtos SET quantidade = %s WHERE id = %s",
+                (id_produtos, nova_quantidade)
+            )
+            conexao.commit()
+        except Exception as erro:
+            print(f"Erro ao tentar atuaizar o produtos {erro}")
         finally:
             cursor.close()
             conexao.close()
