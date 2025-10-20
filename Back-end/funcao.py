f8001ad130f6a0e975d052ecff3de667fd30f0ca
gabriel santos silva <gabriel.s.silva95@aluno.senai.br>
2025-10-20 10:07:39 -0300

    add função inserir_produtos

--- Back-end/funcao.py
+++ Back-end/funcao.py
@@ -5,7 +5,7 @@ def criar_tabela():
     if conexao:
         try:
             cursor.execute("""
-            CREATE TABLE produtos (
+            CREATE TABLE  IF NOT EXISTS produtos (
             id SERIAL PRIMARY KEY,
             nome VARCHAR(100) NOT NULL,
             categoria VARCHAR(50),
@@ -20,3 +20,18 @@ def criar_tabela():
             cursor.close()
             conexao.close()
 criar_tabela()
+
+def inserir_produtos(nome, categoria, preco ,quantidade):
+    conexao, cursor = conectar()
+    if conexao:
+        try:
+            cursor.execute(
+                "INSERT INTO produtos (nome, categoria, preco ,quantidade) VALUES (%s, %s, %s, %s)",
+                (nome, categoria, preco ,quantidade)
+            )
+            conexao.commit()
+        except Exception as erro:
+            print(f"Erro ao inserir filmes {erro}")
+        finally:
+            cursor.close()
+            conexao.close()
