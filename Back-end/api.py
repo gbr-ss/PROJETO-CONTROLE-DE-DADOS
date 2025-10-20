ac06e6f6380e711827f457958230fdbe2e381d39
gabriel santos silva <gabriel.s.silva95@aluno.senai.br>
2025-10-20 10:41:42 -0300

    add api get listar

--- Back-end/api.py
+++ Back-end/api.py
@@ -10,3 +10,17 @@ def home():
 def criar_produto(nome: str, categoria: str, preco: int, quantidade: float):
     funcao.inserir_produtos(nome, categoria, preco ,quantidade)
     return{"mensagem": "Produto adicionado com sucesso"}
+
+@app.get("/produtos")
+def listar_produtos():
+    produtos = funcao.exibir_produtos()
+    lista = []
+    for linha in produtos:
+        lista.append({
+            "id": linha[0],           
+            "nome": linha[1],
+            "categoria": linha[2],
+            "preco": linha[3],
+            "quantidade":linha[4]
+            })
+    return{"produtos": lista}
